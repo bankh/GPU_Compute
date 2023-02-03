8b26e6401370a5a311b934312a1a78c2f13f750d
H.S.Bank <sinanbank@gmail.com>
2023-02-03 14:23:01 -0800

    Update launch.sh
    Comment added

--- docker_images/container-4.3-complete-torch/launch.sh
+++ docker_images/container-4.3-complete-torch/launch.sh
@@ -3,7 +3,7 @@ docker run -it --device=/dev/kfd \
 	       --device=/dev/dri \
                --security-opt seccomp=unconfined \
                --group-add $(getent group render | cut -d':' -f 3) \
-               -v /mnt/data_drive:/mnt/data_drive \
+               -v /mnt/data_drive:/mnt/data_drive \  # to mount /mnt/data_drive to docker container for accessing data
 	       rocm43pytorch
 
 #sudo docker run -it --rm \
