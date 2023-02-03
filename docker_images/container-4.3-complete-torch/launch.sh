5a157170bb364ede1b48764045709f9dfe372f8d
H.S.Bank <sinanbank@gmail.com>
2023-02-03 15:08:06 -0800

    Update launch.sh

--- docker_images/container-4.3-complete-torch/launch.sh
+++ docker_images/container-4.3-complete-torch/launch.sh
@@ -5,13 +5,3 @@ docker run -it --device=/dev/kfd \
                --group-add $(getent group render | cut -d':' -f 3) \
                -v /mnt/data_drive:/mnt/data_drive \  # to mount /mnt/data_drive to docker container for accessing data
 	       rocm43pytorch
-
-#sudo docker run -it --rm \
-#--user $UID \
-#--name rocm43pytorch \
-#--network host \
-#--mount type=bind,source=/dev/sdb,target=/mnt/data_drive\
-#--device /dev/dri:/dev/dri \
-#--device /dev/kfd:/dev/kfd \
-#-v $PWD:/mnt \
-#rocm43pytorch
