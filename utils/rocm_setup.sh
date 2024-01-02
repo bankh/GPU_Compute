76dc091d82afb34fe1d9da91f00483fb04e72182
H.S.Bank <sinanbank@gmail.com>
2024-01-02 02:41:34 -0800

    Update rocm_setup.sh

--- utils/rocm_setup.sh
+++ utils/rocm_setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Reference: https://github.com/tinygrad/tinygrad/blob/master/extra/assembly/rocm/rocm_setup.sh
+# (Similar to standard documentation i.e., https://gpuopen.com/learn/amd-lab-notes/amd-lab-notes-rocm-installation-readme/#option-1)
 rm amdgpu-install_5.5.50500-1_all.deb
 wget https://repo.radeon.com/amdgpu-install/5.5/ubuntu/$(lsb_release -cs)/amdgpu-install_5.5.50500-1_all.deb
 sudo dpkg -i amdgpu-install_5.5.50500-1_all.deb
